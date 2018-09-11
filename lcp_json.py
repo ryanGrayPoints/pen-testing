78a20ab99f23638d56e52448cf614035e0c57b9b
ryanGrayPoints <30698596+ryanGrayPoints@users.noreply.github.com>
2018-09-11 14:07:40 -0400

    Add files via upload

---
+++ lcp_json.py
@@ -0,0 +1,59 @@
+
+import subprocess
+import json
+
+
+########################################################################################################################
+def open_del(filename, open_file=False, del_file=True):
+    if open_file is True:
+        subprocess.call(filename, shell=True)
+    if del_file is True:
+        subprocess.call('del ' + filename, shell=True)
+
+
+########################################################################################################################
+def read_json_doc(json_doc, open_file=False, del_file=True):
+    with open(json_doc, 'r') as json_file:
+        json_object = json.load(json_file)
+    open_del(json_doc, open_file, del_file)
+    return json_object
+
+
+########################################################################################################################
+def json_to_dict(json_doc, keys):
+    json_dict = {}
+    for k in keys:
+        field = k[0].split('.')
+        key = k[1]
+        temp_json = json_doc
+        counter = 0
+
+        while counter < len(field):
+            json_dict[key] = ''
+
+            if field[counter] in temp_json:
+                if len(field) == counter + 1:
+                    try:
+                        json_dict[key] = str(temp_json[field[counter]]).replace(',', '')
+                    except:
+                        json_dict[key] = ''
+                else:
+                    temp_json = temp_json[field[counter]]
+            counter += 1
+    return json_dict
+
+
+########################################################################################################################
+def json_dict_to_str(json_dict):
+    temp_str = str(json_dict)
+    json_str = temp_str.replace('"', '\\"').replace("'", '\\"').replace(', u\\', ', \\').replace(': u\\', ': \\'). \
+        replace('{u\\', '{\\').replace('[u\\', '[\\')
+    return json_str
+
+
+########################################################################################################################
+def json_display(json_dict):
+    json_str = str(json_dict)
+    output_str = json_str.replace("'", '"').replace('{u"', '{"').replace('[u"', '["').replace('', '').replace(', u"',
+                                    ', "').replace(': u"', ': "').replace(',', ',\n').replace('{', '{\n')
+    return output_str
