84eaf847f9b85ad905521c5ec9be0dc411d880d4
ryanGrayPoints <30698596+ryanGrayPoints@users.noreply.github.com>
2018-09-12 17:44:29 -0400

    Add files via upload

--- lcp_json.py
+++ lcp_json.py
@@ -1,6 +1,8 @@
 
 import subprocess
 import json
+import datetime
+import re
 
 
 ########################################################################################################################
@@ -13,9 +15,12 @@ def open_del(filename, open_file=False, del_file=True):
 
 ########################################################################################################################
 def read_json_doc(json_doc, open_file=False, del_file=True):
-    with open(json_doc, 'r') as json_file:
-        json_object = json.load(json_file)
-    open_del(json_doc, open_file, del_file)
+    try:
+        with open(json_doc, 'r') as json_file:
+            json_object = json.load(json_file)
+        open_del(json_doc, open_file, del_file)
+    except:
+        json_object = {}
     return json_object
 
 
@@ -33,3 +38,12 @@ def json_display(json_dict):
     output_str = json_str.replace("'", '"').replace('{u"', '{"').replace('[u"', '["').replace('', '').\
         replace(', u"', ', "').replace(': u"', ': "').replace(',', ',\n').replace('{', '{\n')
     return output_str
+
+
+########################################################################################################################
+def create_filename(prefix):
+    date_str = str(datetime.datetime.now())
+    timestamp = re.sub('[^0-9]', '', date_str)
+    filename = prefix + '_' + timestamp
+    return filename
+
